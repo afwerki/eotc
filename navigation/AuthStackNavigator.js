c695fa2c9dc5c54073b37d6bdb3bd66891e45947
Afwerki Ghirmay <afexgh07@gmail.com>
2024-06-07 01:01:00 +0100

    update

--- navigation/AuthStackNavigator.js
+++ navigation/AuthStackNavigator.js
@@ -1,15 +1,47 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import { createStackNavigator } from '@react-navigation/stack';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 import LoginScreen from '../screens/LoginScreen';
 import RegistrationScreen from '../screens/RegistrationScreen';
+import WelcomeScreen from '../screens/WelcomeScreen';
 
 const Stack = createStackNavigator();
 
 const AuthStackNavigator = () => {
+  const [isFirstLaunch, setIsFirstLaunch] = useState(null);
+
+  useEffect(() => {
+    const checkIfFirstLaunch = async () => {
+      const hasSeenWelcome = await AsyncStorage.getItem('hasSeenWelcome');
+      setIsFirstLaunch(hasSeenWelcome === null);
+    };
+
+    checkIfFirstLaunch();
+  }, []);
+
+  if (isFirstLaunch === null) {
+    return null; // Optionally, return a loading screen here
+  }
+
   return (
-    <Stack.Navigator initialRouteName="Login">
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Registration" component={RegistrationScreen} />
+    <Stack.Navigator initialRouteName={isFirstLaunch ? 'Welcome' : 'Login'}>
+      {isFirstLaunch && (
+        <Stack.Screen
+          name="Welcome"
+          component={WelcomeScreen}
+          options={{ headerShown: false }}
+        />
+      )}
+      <Stack.Screen
+        name="Login"
+        component={LoginScreen}
+        options={{ headerShown: false }}
+      />
+      <Stack.Screen
+        name="Registration"
+        component={RegistrationScreen}
+        options={{ headerShown: false }}
+      />
     </Stack.Navigator>
   );
 };
---
+++ screens/WelcomeScreen.js
@@ -0,0 +1,97 @@
+import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import React from 'react';
+import { widthPercentageToDP as wp, heightPercentageToDP as hp } from 'react-native-responsive-screen';
+import { StatusBar } from 'expo-status-bar';
+import { LinearGradient } from 'expo-linear-gradient';
+import Animated, { FadeInDown } from 'react-native-reanimated';
+import AsyncStorage from '@react-native-async-storage/async-storage'; // Import AsyncStorage
+
+const WelcomeScreen = ({ navigation }) => {
+    const handleGetStarted = async () => {
+        try {
+            await AsyncStorage.setItem('hasSeenWelcome', 'true');
+            navigation.replace('Login');
+        } catch (error) {
+            console.error('Failed to save the data to the storage', error);
+        }
+    };
+
+    return (
+        <View style={styles.container}>
+            <StatusBar style="light" />
+            <Image style={styles.backgroundImage} source={require("../assets/Priests.png")} />
+
+            <LinearGradient
+                colors={['transparent', '#18181b']}
+                style={styles.gradient}
+                start={{ x: 0.5, y: 0 }}
+                end={{ x: 0.5, y: 0.8 }}
+            >
+                <Animated.View entering={FadeInDown.delay(100).springify()} style={styles.textContainer}>
+                    <Text style={[styles.welcomeText, { fontSize: hp(5) }]}>
+                        እንዃን <Text style={styles.highlightedText}>ወደቤተ ክርስቲያን </Text>
+                    </Text>
+                    <Text style={[styles.welcomeText, { fontSize: hp(5) }]}>
+                        በደህና መጡ
+                    </Text>
+                </Animated.View>
+
+                <Animated.View entering={FadeInDown.delay(200).springify()}>
+                    <TouchableOpacity
+                        onPress={handleGetStarted}
+                        style={[styles.button, { height: hp(7), width: wp(80) }]}
+                    >
+                        <Text style={[styles.buttonText, { fontSize: hp(3) }]}>
+                            Get Started
+                        </Text>
+                    </TouchableOpacity>
+                </Animated.View>
+            </LinearGradient>
+        </View>
+    );
+}
+
+export default WelcomeScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'flex-end',
+    },
+    backgroundImage: {
+        height: '100%',
+        width: '100%',
+        position: 'absolute',
+    },
+    gradient: {
+        width: wp(100),
+        height: hp(70),
+        justifyContent: 'flex-end',
+        paddingBottom: 12,
+    },
+    textContainer: {
+        alignItems: 'center',
+    },
+    welcomeText: {
+        color: 'white',
+        fontWeight: 'bold',
+        letterSpacing: 1,
+    },
+    highlightedText: {
+        color: '#f43f5e',
+    },
+    button: {
+        backgroundColor: '#f43f5e',
+        alignItems: 'center',
+        justifyContent: 'center',
+        alignSelf: 'center',
+        borderRadius: 25,
+        borderWidth: 2,
+        borderColor: '#d4d4d8',
+    },
+    buttonText: {
+        color: 'white',
+        fontWeight: 'bold',
+        letterSpacing: 2,
+    },
+});
